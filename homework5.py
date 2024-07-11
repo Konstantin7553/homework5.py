f46a49f1051a7024491736500c8f1a04b29a00d9
Konstantin7553 <krasik150@gmail.com>
2024-07-11 14:47:33 +0300

    Add files via upload

--- homework5.py
+++ homework5.py
@@ -1,8 +1,9 @@
 tuple_ = 1, 2, 'a', 'b'
 print(tuple_)
 #tuple_ =[1] = 150
+#print(tuple_)
+#Ошибка сообщает, что кортеж не поддерживает обращение к элементам
+tuple_ = ([1, 2, 0])
 print(tuple_)
-tuple_ = ([1, 2], 0)
-print(tuple_)
-tuple_[0] [1] = 4
+tuple_[0][0] = 4
 print(tuple_)
